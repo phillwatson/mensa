2dc7fde601b6a5f4c7db06a120a01c3dd46d0825
Phill Watson <watson.phill@gmail.com>
2022-06-13 09:43:37 +0100

    new user events

--- mensa-email-service/src/main/java/com/hillayes/mensa/email/domain/EmailRecord.java
+++ mensa-email-service/src/main/java/com/hillayes/mensa/email/domain/EmailRecord.java
@@ -1,4 +1,11 @@
 package com.hillayes.mensa.email.domain;
 
+import java.time.Instant;
+
 public class EmailRecord {
+    public String sender;
+
+    public String recipient;
+
+    public Instant dateCreated;
 }
--- mensa-email-service/src/main/java/com/hillayes/mensa/email/service/EmailService.java
+++ mensa-email-service/src/main/java/com/hillayes/mensa/email/service/EmailService.java
@@ -2,11 +2,13 @@ package com.hillayes.mensa.email.service;
 
 import com.hillayes.mensa.events.events.UserCreated;
 import com.hillayes.mensa.events.events.UserOnboarded;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 import javax.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
+@RequiredArgsConstructor
 @Slf4j
 public class EmailService {
     public void sendEmail(UserCreated userCreated) {
